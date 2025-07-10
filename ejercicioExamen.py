02116563e92f1d813e5a7693b23f53ed509acd9c
KazzyUwU <160556807+KazzyUwU@users.noreply.github.com>
2025-07-10 11:58:11 -0400

    Add files via upload

---
+++ ejercicioExamen.py
@@ -0,0 +1,27 @@
+stock_marca = {'acer': [43], 'asus': [3], 'hp': [31], 'dell': [1]}
+
+
+productos = {'8475HD': ['HP', 15.6, '8GB', 'DD', '1T', 'Intel Core i5', 'Nvidia GTX1050'],
+'2175HD': ['Acer', 14, '4GB', 'SSD', '512GB', 'Intel Core i5', 'Nvidia GTX1050'],
+'JjfFHD': ['Asus', 14, '16GB', 'SSD', '256GB', 'Intel Core i7', 'Nvidia RTX2080Ti'],
+'fgdxFHD': ['HP', 15.6, '12GB', 'DD', '1T', 'Intel Core i3', 'integrada'],
+'GF75HD': ['Asus', 15.6, '12GB', 'DD', '1T', 'Intel Core i7', 'Nvidia GTX1050'],
+'123FHD': ['Acer', 14, '6GB', 'DD', '1T', 'AMD Ryzen 5', 'integrada'],
+'342FHD': ['Acer', 15.6, '8GB', 'DD', '1T', 'AMD Ryzen 7', 'Nvidia GTX1050'],
+'UWU131HD': ['Dell', 15.6, '8GB', 'DD', '1T', 'AMD Ryzen 3', 'Nvidia GTX1050'], 
+}
+stock = {'8475HD': [387990,10],'2175HD': [327990,4], 'JjfFHD': [424990,1], 'fgdxFHD': [664990,21], '123FHD': [290890,32], '342FHD': [444990,7],
+'GF75HD': [749990,2], 'UWU131HD': [349990,1], 'FS1230HD': [249990,0]}
+
+print("BIENVENIDO AL MENU")
+print("1. Stock")
+print("2. Busqueda por precio")
+print("3. Eliminar producto")
+print("4. Salir")
+
+input("ingrese opcion: ")
+
+case:1
+marca=input("ingrese marca a consultar: ")
+
+print("toy cagado profe :c")
